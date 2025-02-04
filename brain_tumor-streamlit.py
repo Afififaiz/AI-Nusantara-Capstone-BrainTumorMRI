6b3108728714fe6b4112f2a1b9277354d29cf48b
You can call me PIPOT <139671586+Afififaiz@users.noreply.github.com>
2025-02-04 18:02:06 +0800

    Update brain_tumor-streamlit.py

--- brain_tumor-streamlit.py
+++ brain_tumor-streamlit.py
@@ -109,7 +109,7 @@ def get_description(predicted_class):
 
 # Homepage
 def homepage():
-    st.image(r"home.PNG", use_column_width=True)
+    st.image(r"home.PNG", use_container_width=True)
 
     st.markdown("""
         <style>
@@ -165,7 +165,7 @@ def homepage():
 
     st.write("")
     st.write("")
-    st.image(r"home2.PNG", use_column_width=True)
+    st.image(r"home2.PNG", use_container_width=True)
 
        # Use session state for page navigation
     if st.button("Make Predictions"):
@@ -174,7 +174,7 @@ def homepage():
 
 # Main Prediction Page
 def prediction_page():
-    st.image(r"ai.PNG", use_column_width=True)
+    st.image(r"ai.PNG", use_container_width=True)
     st.write("Upload an image to classify. You can have the image through the button below. Download it and predict")
 
     if st.button("Go Download a sample"):
@@ -191,7 +191,7 @@ def prediction_page():
         image = Image.open(uploaded_file)
         image = image.resize((224, 224))  # Resize the image to 224x224 pixels
         image = image.convert("RGB")  # Ensure the image is in RGB format
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
         # Display loading message
         with st.spinner('Classifying...'):
@@ -211,7 +211,7 @@ def prediction_page():
 
 # History Page
 def history_page():
-    st.image(r"his.PNG", use_column_width=True)
+    st.image(r"his.PNG", use_container_width=True)
 
     # Check if there's any history
     if 'history' in st.session_state and st.session_state['history']:
@@ -251,7 +251,7 @@ def list_categories_and_images(extract_dir):
         if selected_image:
             image_path = os.path.join(image_dir, selected_image)
             image = Image.open(image_path)
-            st.image(image, caption=f"{selected_category} - {selected_image}", use_column_width=True)
+            st.image(image, caption=f"{selected_category} - {selected_image}", use_container_width=True)
             
             # Provide download link
             with open(image_path, "rb") as file:
@@ -263,7 +263,7 @@ def list_categories_and_images(extract_dir):
                 )
 
 def dataset_page():
-    st.image(r"data.PNG", use_column_width=True)
+    st.image(r"data.PNG", use_container_width=True)
     extract_dir = download_and_extract_zip()
     list_categories_and_images(extract_dir)
 
@@ -273,7 +273,7 @@ def main():
     if 'page' not in st.session_state:
         st.session_state.page = "Home"
 
-    st.sidebar.image(r"ailogo.PNG", use_column_width=True)
+    st.sidebar.image(r"ailogo.PNG", use_container_width=True)
 
     st.sidebar.title("Navigation")
 
